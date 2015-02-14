162215a4b38d7e60c044bae4a46f36274203ddb2
Guro Bokum <jiojiajiu@gmail.com>
2015-02-14 20:29:57 +0700

    signUpChampion controller

---
+++ frontend/js/controllers/signUpChampion.js
@@ -0,0 +1,86 @@
+(function (undefined) {
+'use strict';
+
+appControllers.controller('signUpChampionCtrl', [
+    '$scope',
+    '$location',
+    '$rootScope',
+    '$log',
+    'formUtils',
+function ($scope, $location, $rootScope, $log, formUtils) {
+
+    $scope.resetGlobal({
+        headless: true
+    });
+
+    $scope.requiredFields = {
+        firstName: {
+            fieldName: 'firstName',
+            describeName: 'First Name'
+        },
+        lastName: {
+            fieldName: 'lastName',
+            describeName: 'Last Name'
+        },
+        mail: {
+            fieldName: 'mail',
+            describeName: 'Email Address'
+        },
+        password: {
+            fieldName: 'password',
+            describeName: 'Password'
+        },
+        confirmPassword: {
+            fieldName: 'confirmPassword',
+            describeName: 'Confirm Password'
+        }
+    };
+
+    // reset modal
+    //signUpUtil($scope, $location, $rootScope);
+    $scope.submit = function () {
+        $log.log('Submitting..');
+
+        if (!validate()) {
+            $log.log('Form is not valid');
+            return;
+        }
+    };
+
+    $scope.toggleAcceptTerms = function () {
+        $scope.isAcceptTerms = !$scope.isAcceptTerms;
+    };
+
+    function validate () {
+        var valid = formUtils.validateRequired($scope, $scope.signUpForm);
+        if (!valid)
+            return valid;
+
+        valid = formUtils.matchPasswords($scope) && valid;
+        if (!valid)
+            return valid;
+
+        if ($scope.signUpForm.mail.$dirty && $scope.signUpForm.mail.$invalid) {
+            $scope.validateErrorMsg += 'Email is invalid.';
+            return false;
+        }
+
+        $scope.afterClickRegister = true;
+        return false;
+    }
+
+    // focus input
+    $scope.focusInput = function () {
+        // if just right after click register, clear error message.
+        if ($scope.afterClickRegister) {
+            $scope.showError = false;
+            $scope.validateErrorMsg = "";
+            $scope.afterClickRegister = false;
+            // clear all warning border
+            $("input").removeClass('invalid');
+        }
+    };
+    $scope.looseFocus = formUtils.looseFocus;
+}]);
+
+})();
