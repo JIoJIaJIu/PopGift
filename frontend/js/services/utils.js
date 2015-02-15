ca0b64ded93a002f22c2ccc9d4128113394bac82
Guro Bokum <jiojiajiu@gmail.com>
2015-02-15 12:14:03 +0700

    Utils

--- frontend/js/services/utils.js
+++ frontend/js/services/utils.js
@@ -1,3 +1,15 @@
 (function (undefined) {
+'use strict';
+
+angular.module('MomAndPop').service('utils', function () {
+    this.pathJoin = function () {
+        var components = [];
+        components = components.concat(components, _.map(arguments, function (node) {
+            return node.split('/');
+        });
+
+        return components.join('/');
+    }
+});
 
 })();
