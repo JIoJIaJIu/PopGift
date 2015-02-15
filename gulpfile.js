dfb1b06c031ad23f01fa50b3b718b99a7c264943
Guro Bokum <jiojiajiu@gmail.com>
2015-02-15 11:13:24 +0700

    New assembly

--- gulpfile.js
+++ gulpfile.js
@@ -10,31 +10,46 @@ var path = require('path');
 var OUTPUT = 'build';
 var OUTPUT_JS = path.join(OUTPUT, 'js');
 
-gulp.task('default', ['third-parties', 'build', 'templates']);
+gulp.task('default', ['third-parties', 'build', 'templates', 'static']);
 
 gulp.task('build', function () {
-    gulp.src('frontend/*.js')
+    gulp.src(['frontend/js/**/*.js', '!frontend/js/third-parties/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
+        .pipe(concat('app.js'))
         .pipe(gulp.dest(OUTPUT_JS))
+        /*
         .pipe(rename({extname: '.min.js'}))
         .pipe(uglify())
         .pipe(gulp.dest(OUTPUT_JS))
+        */
 });
 
 gulp.task('third-parties', function (cb) {
-    exec('./node_modules/bower/bin/bower install', cb)
+    gulp.src('frontend/js/third-parties/*.js')
+        .pipe(gulp.dest(path.join(OUTPUT_JS, 'third-parties')))
 });
 
 gulp.task('templates', function () {
     gulp.src('frontend/index.html')
         .pipe(gulp.dest(OUTPUT))
 
-    gulp.src('frontend/templates/**')
-        .pipe(gulp.dest(OUTPUT_JS))
+    gulp.src('frontend/templates/pages/**')
+        .pipe(gulp.dest(path.join(OUTPUT, 'views')) );
+
+    gulp.src('frontend/templates/partials/**')
+        .pipe(gulp.dest(path.join(OUTPUT, 'partials')) );
+});
+
+gulp.task('static', function () {
+    gulp.src('frontend/css/**')
+        .pipe(gulp.dest(path.join(OUTPUT, 'css')) );
+
+    gulp.src('frontend/images/**')
+        .pipe(gulp.dest(path.join(OUTPUT, 'i')) );
 });
 
 gulp.task('watch', function () {
-    gulp.watch('frontend/**.html', ['templates']);
-    gulp.watch('frontend/**.js', ['build']);
+    gulp.watch('frontend/templates/**.html', ['templates']);
+    gulp.watch('frontend/js/**/*.js', ['build']);
 });
