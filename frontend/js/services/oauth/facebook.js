38e3767ba6adece798448ac71a3bc4264a992f0d
Guro Bokum <jiojiajiu@gmail.com>
2015-02-15 12:59:20 +0700

    OAuth facebook service

---
+++ frontend/js/services/oauth/facebook.js
@@ -0,0 +1,12 @@
+(function (undefined) {
+
+angular.module('MomAndPop').service([
+    '$log',
+function ($log) {
+    $log.info('Facebook oauth service');
+
+    this.connect = function () {
+    }
+}]);
+
+})();
